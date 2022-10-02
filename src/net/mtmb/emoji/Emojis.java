0a66e1bb50a8b998e6d73a163974adc66ade9626
Max <max@maximilian.co.nz>
2022-10-02 20:09:51 +1300

    Added more emojis

--- src/net/mtmb/emoji/Emojis.java
+++ src/net/mtmb/emoji/Emojis.java
@@ -11,7 +11,13 @@ public class Emojis {
         {":skull:", ":sku"},
         {":sob:", ":sob"},
         {":nerd:", ":ner"},
-        {":joy:", ":joy"}
+        {":joy:", ":joy"},
+        {":triumph:", ":tri"},
+        {":pensive:", ":pe"},
+        {":joy_cat:", ":joyc"},
+        {":exploding:", ":exp"},
+        {":nauseated:", ":nau"},
+        {":yawn:", ":yaw"}
     };
 
     // Add all the emojis
@@ -25,6 +31,12 @@ public class Emojis {
         emojis.put("\uE002", emojiNames[2]);
         emojis.put("\uE003", emojiNames[3]);
         emojis.put("\uE004", emojiNames[4]);
+        emojis.put("\uE005", emojiNames[5]);
+        emojis.put("\uE006", emojiNames[6]);
+        emojis.put("\uE007", emojiNames[7]);
+        emojis.put("\uE008", emojiNames[8]);
+        emojis.put("\uE009", emojiNames[9]);
+        emojis.put("\uE010", emojiNames[10]);
         
         return emojis;
     }
