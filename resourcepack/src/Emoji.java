588c5060011f36494b0ae7e115a4df7cfbeb79d3
MaximilianMcC <max@maximilian.co.nz>
2023-03-07 07:19:04 +1100

    Added hex, and filename to the emoji

--- resourcepack/src/Emoji.java
+++ resourcepack/src/Emoji.java
@@ -4,13 +4,17 @@ public class Emoji {
     
     public String shortcode;
     public String imageUrl;
+    public String hex;
+    public String filename;
 
     // Emoji constructor
-    public Emoji(String shortcode, String url) {
+    public Emoji(String shortcode, String hex, String url) {
 
         // Assign all variables
         this.shortcode = shortcode;
         this.imageUrl = "https://www.emojibase.com" + url;
+        this.hex = hex;
+        this.filename = shortcode.replaceAll(":", "");
     }
 
 }
